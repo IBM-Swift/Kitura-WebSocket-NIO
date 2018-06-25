839417c0592acd349be7c79ff64a34a17b8ed89d
Nethra Ravindran <nethra.ravindran@in.ibm.com>
2018-06-25 15:03:20 +0530

    Added copyright headers and dummy test

--- Tests/KituraWebSocketNIOTests/Kitura_WebSocket_NIOTests.swift
+++ Tests/KituraWebSocketNIOTests/Kitura_WebSocket_NIOTests.swift
@@ -17,12 +17,13 @@
 import XCTest
 @testable import KituraWebSocketNIO
 
-final class Kitura_WebSocket_NIOTests: XCTestCase {
+public class Kitura_WebSocket_NIOTests: XCTestCase {
+    static let text: String = "Hello, World!"
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Kitura_WebSocket_NIO().text, "Hello, World!")
+        XCTAssertEqual(Kitura_WebSocket_NIOTests.text, "Hello, World!")
     }
 
 
